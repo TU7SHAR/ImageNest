4209aa2290b21aff6607f5d342000ba4453439f7
Tushar Gautam <109740805+TU7SHAR@users.noreply.github.com>
2026-04-14 10:48:51 +0530

    Update HomePage.jsx

--- app/components/HomePage.jsx
+++ app/components/HomePage.jsx
@@ -87,12 +87,11 @@ function HomePage() {
             </div>
           </div>
         </section>
-{/* chat.bot Widget */}
-<Script 
-  src="https://chatbot-c53nl.ondigitalocean.app/static/js/embed.js" 
-  data-bot-id="15" 
-  strategy="afterInteractive" 
-/>
+        {/* BotFactory Widget */}
+<Script id="botfactory-init" strategy="beforeInteractive">
+  {`window.BOTFACTORY_ID = "15";`}
+</Script>
+<Script src="https://chatbot-c53nl.ondigitalocean.app/static/js/embed.js" strategy="afterInteractive" />
       </div>
     </>
   );
