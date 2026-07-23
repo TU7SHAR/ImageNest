435c5e792f33add310987986571cc45fdcade455
Tushar Gautam <109740805+TU7SHAR@users.noreply.github.com>
2026-07-23 16:04:18 +0530

    Integrate Bubbl Widget into Home component
    Added Bubbl Widget scripts to the Home component.

--- app/page.js
+++ app/page.js
@@ -1,9 +1,18 @@
+import Script from "next/script";
 import HomePage from "./components/HomePage";
 
 export default function Home() {
   return (
     <>
       <HomePage />
+      {/* Bubbl Widget */}
+      <Script id="bubbl-init" strategy="beforeInteractive">
+        {`window.BOTFACTORY_ID = "8";`}
+      </Script>
+      <Script
+        src="https://app.bubbl.ooo/static/js/embed.js"
+        strategy="afterInteractive"
+      />
     </>
   );
 }
