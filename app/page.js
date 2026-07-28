0481e6541658bf1e2afa402f7002fe4df428e79a
Tushar Gautam <109740805+TU7SHAR@users.noreply.github.com>
2026-07-28 15:19:50 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -6,13 +6,12 @@ export default function Home() {
     <>
       <HomePage />
       {/* Bubbl Widget */}
-      <Script id="bubbl-init" strategy="beforeInteractive">
-        {`window.BOTFACTORY_ID = "8";`}
-      </Script>
-      <Script
-        src="https://app.bubbl.ooo/static/js/embed.js"
-        strategy="afterInteractive"
-      />
+ {/* Bubbl Widget */}
+<Script id="bubbl-init" strategy="beforeInteractive">
+  {`window.BOTFACTORY_ID = "10";`}
+</Script>
+<Script src="https://app.bubbl.ooo/static/js/embed.js"
+  strategy="afterInteractive" />
     </>
   );
 }
